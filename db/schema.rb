c17502b047abbe30665a84aa9974827c7304c2d5
John Arboleda <JohnJack0126@gmail.com>
2021-05-06 15:44:18 -0500

    Build Comment model and asociations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_194112) do
+ActiveRecord::Schema.define(version: 2021_05_06_203546) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
