c954917b92d9737ffc426f8004575ebd1716ec2c
John Arboleda <JohnJack0126@gmail.com>
2021-05-06 12:33:53 -0500

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence:true, length: { minimum: 4 }, length: { minimum: 2 }
-  
+  validates :username, presence:true, length: { minimum: 4 }, uniqueness: true
+  validates :email, presence:true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, 
+    message: "need to enter a valid email"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_162826) do
+ActiveRecord::Schema.define(version: 2021_05_06_170259) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_162826) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
