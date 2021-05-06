e66f8b7d46ae6d1196347f74bba47d9586269459
John Arboleda <JohnJack0126@gmail.com>
2021-05-06 16:46:59 -0500

    Fix rubocop end of line error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
-    validates :content, presence: true, length: { minimum: 1, maximum: 300  }
-    validates :user_id, presence: true
-    validates :post_id, presence: true
-    
+  validates :content, presence: true, length: { minimum: 1, maximum: 300 }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-    validates :title, presence:true, length: { minimum: 3 }, format: {with: /\A\S+/, message: "cannot begin with white space"}
-    validates :body, presence:true, length: { minimum: 10 }
+  validates :title, presence: true, length: { minimum: 3 },
+                    format: { with: /\A\S+/, message: 'cannot begin with white space' }
+  validates :body, presence: true, length: { minimum: 10 }
 
-    belongs_to :user
-    has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_many :comments, dependent: :destroy
 end
