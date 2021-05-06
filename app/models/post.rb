02dd92bfa1fd3c62ad12080ea5ba8e353807fb53
Maha Magdy <mahamagdy3@gmail.com>
2021-05-06 22:17:01 +0200

    add associations relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence:true, length: { minimum: 3 }, format: {with: /\A\S+/, message: "cannot begin with white space"}
+    validates :body, presence:true, length: { minimum: 10 }
+
+    belongs_to :user
+    has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :username, presence:true, length: { minimum: 4 }, uniqueness: true
   validates :email, presence:true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, 
     message: "need to enter a valid email"}
+
+  has_many :posts
+  has_many :comments, through: :posts
 end
